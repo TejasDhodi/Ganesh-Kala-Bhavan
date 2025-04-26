f7c754cfdb781d0806a1e88ec256aa445319c640
Tejas Dhodi <tejasdhodi77@gmail.com>
2025-04-26 21:50:41 +0530

    change puppeteer launch  configuration

--- Server/Controllers/generateReceipt.controller.js
+++ Server/Controllers/generateReceipt.controller.js
@@ -71,7 +71,10 @@ const generateReceipt = async (req, res) => {
 
 
 const generatPdf = async ({ name, email, phone, amount, items }) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+  });
   const page = await browser.newPage();
 
   const html = `
